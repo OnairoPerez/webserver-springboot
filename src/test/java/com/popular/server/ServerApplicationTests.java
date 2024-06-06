ec04518dedbf979abb547ebf88d8fd2c86b7ad4c
Onairo Pérez <onairoperez48@gmail.com>
2024-06-06 18:36:54 -0500

    Project start

---
+++ src/test/java/com/popular/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popular.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
